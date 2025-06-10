52e53eaddd41235d0d58bce28bcba2954fa4232e
Garry Caber <garrypedrosa.31@gmail.com>
2025-06-10 16:59:04 +0800

    Add Custom Web Cursor

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,27 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <router-view/>
+  <router-view />
+  <h1 style="cursor: url('/cursors/cursor-default.cur'), auto">
+    Default cursor here
+  </h1>
+  <h1 style="cursor: url('/cursors/cursor-pointer.cur'), pointer">
+    Pointer cursor here
+  </h1>
+  <h1 style="cursor: url('/cursors/cursor-text.cur'), text">
+    Text cursor here
+  </h1>
+  <h1 style="cursor: url('/cursors/cursor-code.cur'), auto">
+    Code cursor here
+  </h1>
+  <h1 style="cursor: url('/cursors/cursor-adventure.cur'), auto">
+    Adventure cursor here
+  </h1>
 </template>
 
-<style scoped>
+<style>
+html,
+body {
+  cursor: url("/cursors/cursor-default.cur"), not-allowed;
+}
 </style>
